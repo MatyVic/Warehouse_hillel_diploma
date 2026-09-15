5ac9543b6fb72a21a0d64b487848fd84cbbf921d
MatyVic <thexcvb@gmail.com>
2026-09-15 11:12:06 +0300

    Add models for warehouse

--- warehouse/models.py
+++ warehouse/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100, verbose_name=_("Title"))
+    authors = models.CharField(max_length=255, verbose_name=_("Authors"))
+    category = models.CharField(max_length=100, verbose_name=_("Category"))
+    publisher = models.CharField(max_length=100, verbose_name=_("Publisher")
+    )
+    published_year = models.IntegerField(verbose_name=_("Published year"))
+    amount = models.IntegerField(verbose_name=_("Amount"))
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name=_("Price")
+    )
+    available = models.BooleanField(default=True, verbose_name=_("Available"))
+
+class Warehouse(models.Model):
+    pass
+
+
+
+class WarehouseCells(models.Model):
+    pass
