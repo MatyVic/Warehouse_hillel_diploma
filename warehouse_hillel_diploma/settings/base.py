24893b8d376e76ecfcc07e84ebfe83019cde9c69
MatyVic <thexcvb@gmail.com>
2026-09-16 12:43:11 +0300

    Add docker files

--- warehouse_hillel_diploma/settings/base.py
+++ warehouse_hillel_diploma/settings/base.py
@@ -101,7 +101,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "order.context_processors.cart_count",
+                #"order.context_processors.cart_count",
                 "django.template.context_processors.i18n",
             ],
         },
