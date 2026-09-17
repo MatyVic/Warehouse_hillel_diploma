7e27463fb091bd0236e9ec1a7f997dee8db8d1d6
MatyVic <thexcvb@gmail.com>
2026-09-17 10:01:58 +0300

    Add DRF classes

--- warehouse_api/views.py
+++ warehouse_api/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, serializers, permissions
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework.throttling import UserRateThrottle
 
-# Create your views here.
+from warehouse.models import Book, Stock
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+class BooksLimitOffsetPagination(PageNumberPagination):
+    page_size = 50
+    page_size_query_param = "page_size"
+    max_page_size = 50
+
+
+class StockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stock
+        fields = '__all__'
+
+
+class StockLimitOffsetPagination(PageNumberPagination):
+    page_size = 50
+    page_size_query_param = "page_size"
+    max_page_size = 50
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    pagination_class = BooksLimitOffsetPagination
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    class Meta:
+        model = Book
+        fields = "__all__"
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAdminUser()]
+        return [permissions.AllowAny()]
+
+
+class StockViewSet(viewsets.ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend]
